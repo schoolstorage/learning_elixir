5c466cb90aa1b24b2253720d1dc24be8901ae604
schoolstorage <newton.sarker@ut.ee>
2021-09-07 22:07:20 +0300

    updated recursion

--- exercise1/lib/reverse.ex
+++ exercise1/lib/reverse.ex
@@ -1,13 +1,12 @@
 defmodule Exercise1.Reverse do
 
   def reverse(chararray) do
-    reverse_char_list(chararray, [])
+    reverse_char_list(chararray)
   end
 
-  defp reverse_char_list([head | tail], new_list) do
-    reverse_char_list(tail, [head] ++ new_list)
+  defp reverse_char_list([]), do: []
+  defp reverse_char_list([head | tail]) do
+    reverse_char_list(tail) ++ [head]
   end
-
-  defp reverse_char_list([], new_list), do: new_list
 
 end
