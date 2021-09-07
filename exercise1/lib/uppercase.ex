f546cf602cab66058cbaec2351c6ef8b1c8b3c44
schoolstorage <newton.sarker@ut.ee>
2021-09-07 21:50:36 +0300

    updated recursion

--- exercise1/lib/uppercase.ex
+++ exercise1/lib/uppercase.ex
@@ -1,15 +1,13 @@
 defmodule Exercise1.Uppercase do
 
   def upcase(chararray) do
-    convert_to_upper_case(chararray, [])
+    scroll(chararray, fn (head) -> to_upper_case(head) end)
   end
 
-  def convert_to_upper_case([head | tail], modified_list) do
-
-    convert_to_upper_case(tail, modified_list ++ [to_upper_case(head)])
+  def scroll([], fun), do: []
+  def scroll([head | tail], fun) do
+    [fun.(head) | scroll(tail, fun)]
   end
-
-  def convert_to_upper_case([], modified_list), do: modified_list
 
   def to_upper_case(lowercase_char) do
     char_map = %{97 => 65, 98 => 66, 99 => 67, 100 => 68, 101 => 69, 102 => 70, 103 => 71, 104 => 72, 105 => 73, 106 => 74, 107 => 75, 108 => 76, 109 => 77, 110 => 78, 111 => 79, 112 => 80, 113 => 81, 114 => 82, 115 => 83, 116 => 84, 117 => 85, 118 => 86, 119 => 87, 120 => 88, 121 => 89, 122 => 90}
