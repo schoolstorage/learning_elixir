c6ceda0cde0acc91560f969277efc2b9c093ff55
schoolstorage <newton.sarker@ut.ee>
2021-09-21 08:05:25 +0300

    updated pattern matching

--- exercise1/lib/nonalpha.ex
+++ exercise1/lib/nonalpha.ex
@@ -1,15 +1,11 @@
 defmodule Exercise1.NonAlpha do
 
   def remove_non_alpha(chararray) do
-    Exercise1.MList.foldl(chararray, [], fn(head, acc) -> clean_char(head, acc) end)
+    clean_list(chararray)
   end
 
-  def clean_char(char_code, acc) do
-    if (char_code >= 65 && char_code <= 90) || (char_code >= 97 && char_code <= 122) do
-      acc ++ [char_code]
-    else
-      acc
-    end
-  end
+  defp clean_list([]), do: []
+  defp clean_list([head | tail]) when head in ?a .. ?z, do: [head - 32 | clean_list(tail)]
+  defp clean_list([head | tail]), do: [head | clean_list(tail)]
 
 end
