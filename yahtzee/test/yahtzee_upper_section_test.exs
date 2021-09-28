03de7b942ff29f3f8f07a109683660846a78f390
schoolstorage <newton.sarker@ut.ee>
2021-09-28 21:33:55 +0300

    [Part 0 - Fail] second test added

--- yahtzee/test/yahtzee_upper_section_test.exs
+++ yahtzee/test/yahtzee_upper_section_test.exs
@@ -1,8 +1,21 @@
 defmodule YahtzeeUpperSectionTest do
   use ExUnit.Case
 
+  def generate(die_face, occurrences) do
+    Enum.to_list(1..6)
+    |> List.delete(die_face)
+    |> Enum.shuffle
+    |> Enum.take(5 - occurrences)
+    |> Enum.concat(List.duplicate(die_face, occurrences))
+    |> Enum.shuffle
+  end
+
   test "works with 1 one" do
     assert %{Ones: 1} = Yahtzee.score_upper([1,2,3,4,5])
+  end
+
+  test "works with any combination of dice, containing 1-5 ones" do
+    Enum.map(1..5, fn n -> assert %{Ones: ^n} = Yahtzee.score_upper(generate(1, n)) end)
   end
 
 end
