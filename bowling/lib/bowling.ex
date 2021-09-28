ebfc9f7f05ccb1f3fb73535316ca2438588e5471
schoolstorage <newton.sarker@ut.ee>
2021-09-28 18:27:00 +0300

    initial project structure

--- bowling/lib/bowling.ex
+++ bowling/lib/bowling.ex
@@ -1,18 +1,61 @@
 defmodule Bowling do
 
   def score(game) do
-    List.flatten(game)
-    |> Enum.filter( fn(single_score) -> is_number(single_score) end )
+    clean_scores(game)
     |> calculate_score
   end
 
-  #def calculate_score(cleaned_scores) do
-  #  Enum.reduce( cleaned_scores, 0, fn(single_score, accumulator) -> single_score + accumulator end )
-  #end
+  def clean_scores(game) do
+    List.flatten(game)
+    |> Enum.filter( fn(single_score) -> is_number(single_score) end )
+  end
 
-  def calculate_score([ 10 | rest ]), do: calculate_score(rest) + List.first(rest)
-  def calculate_score([ score1 | [ score2 | rest ] ]) when score1 + score2 == 10, do: calculate_score(rest) + score1 + score2 + List.first(rest)
-  def calculate_score([ score1 | [ score2 | rest ] ]), do: calculate_score(rest) + score1 + score2
-  def calculate_score([]), do: 0
+  def calculate_score([ 10 | [ score2 | rest ] ]) do
+    IO.inspect 'calculate_score([ 10 | [ score2 | rest ] ])'
+    IO.inspect 10
+    IO.inspect score2
+    IO.inspect rest
+    IO.inspect 10 + score2 + List.first(rest)
+    10 + score2 + List.first(rest) + calculate_score(rest)
+  end
+
+  def calculate_score([ score1 | [ score2 | rest ] ]) when score1 + score2 == 10 do
+    IO.inspect 'processing calculate_score([ score1 | [ score2 | rest ] ]) when score1 + score2 == 10'
+    IO.inspect score1
+    IO.inspect score2
+    IO.inspect rest
+    IO.inspect score1 + score2 + List.first(rest)
+    score1 + score2 + List.first(rest) + calculate_score(rest)
+  end
+
+  def calculate_score([ score1 | [ score2 | rest ] ]) do
+    IO.inspect 'processing calculate_score([ score1 | [ score2 | rest ] ])'
+    IO.inspect score1
+    IO.inspect score2
+    IO.inspect rest
+    IO.inspect score1 + score2
+    score1 + score2 + calculate_score(rest)
+  end
+
+  def calculate_score([ score1 | [ score2 | [] ] ]) do
+    IO.inspect 'processing calculate_score([ score1 | [ score2 | rest ] ])'
+    IO.inspect score1
+    IO.inspect score2
+    IO.inspect []
+    score1 + score2
+  end
+
+  def calculate_score([ score1 | [] ]) do
+    IO.inspect 'processing calculate_score([ score1 | [ score2 | rest ] ])'
+    IO.inspect score1
+    IO.inspect []
+    score1
+  end
+
+  def calculate_score([]) do
+    IO.inspect 'processing calculate_score([])'
+    IO.inspect []
+    0
+  end
 
 end
--- bowling/test/bowling_test.exs
+++ bowling/test/bowling_test.exs
@@ -16,4 +16,10 @@ defmodule BowlingTest do
     assert Bowling.score(game) == 16
   end
 
+  test "one strike" do
+    game = [[10,nil],[3,4]] ++ List.duplicate([0, 0], 7) ++ [[0, 0, nil]]
+    IO.inspect Bowling.clean_scores(game)
+    assert Bowling.score(game) == 24
+  end
+
 end
